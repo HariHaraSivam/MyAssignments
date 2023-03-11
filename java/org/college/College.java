d9fbe01ca754026648da267a70732d30200d9e40
HariHaraSivam <126374864+HariHaraSivam@users.noreply.github.com>
2023-03-12 03:26:26 +0530

    Add files via upload
    Week3day1

---
+++ java/org/college/College.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("St.Jospeh's");
+	}
+	public void collegeCode() {
+		System.out.println("3123");
+	}
+	public void collegeRank() {
+		System.out.println("1st");
+	}
+}
---
+++ java/week3/day1/MultipleLanguage.java
@@ -0,0 +1,8 @@
+package week3.day1;
+
+public abstract class MultipleLanguage {
+public void phyton() {
+	System.out.println("Phyton Coding");
+}
+public abstract void ruby();
+}
